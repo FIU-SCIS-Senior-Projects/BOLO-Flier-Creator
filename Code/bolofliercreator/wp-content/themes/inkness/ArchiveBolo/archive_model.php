85053202d84cff9c594a861bac8a273be797b3c6
yflei001 <yflei001@fiu.edu>
2015-06-04 21:22:46 -0400

    Update archive_model for new design 1.0
    add the query to retrieve the archive BOLOs.

--- Code/bolofliercreator/wp-content/themes/inkness/ArchiveBolo/archive_model.php
+++ Code/bolofliercreator/wp-content/themes/inkness/ArchiveBolo/archive_model.php
@@ -48,4 +48,51 @@ SQL;
 	}
 }//end class archive_model
 
+
+/**
+	 * gets 24 bolos for the specified agency
+	 * @param $agency_name for the name of the agency, to select the last 24 bolos overal enter $agency_name = 'Show ALL'
+	 * @param offset for offset from the most recent bolo archive: 0 will get the most recent 24, offset = 24 would
+	 * 			get bolos 24 to 48, and so on
+	 */
+	public function get_data($agency_name, $offset){		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "bolo_creator";
+				
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+		
+		if($agency_name == 'Show All' || $agency_name ==''){
+			$sql = <<<SQL
+	    SELECT *
+	    FROM `wp_flierform` 
+	    WHERE archive = TRUE
+	    ORDER BY edit_date DESC LIMIT $offset,24
+SQL;
+		}
+		else{
+			$sql = <<<SQL
+	    SELECT *
+	    FROM `wp_flierform` 
+	    WHERE archive = TRUE AND agency="$agency_name"
+	    ORDER BY edit_date DESC LIMIT $offset,24
+SQL;
+		}
+		
+		if(!$result = $conn->query($sql)){
+		    die('There was an error running the query [' . $db->error . ']');
+		}
+		
+		mysqli_close($conn);
+	
+	return $result;
+	}//end get_data
+	
+
 ?>
