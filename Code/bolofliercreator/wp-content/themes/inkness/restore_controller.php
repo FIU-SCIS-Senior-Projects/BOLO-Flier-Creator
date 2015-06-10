b0684331b37015ff3cb7a0299a1aa4000781e735
yflei001 <yflei001@fiu.edu>
2015-06-10 17:54:40 -0400

    Initial code

--- Code/bolofliercreator/wp-content/themes/inkness/restore_controller.php
+++ Code/bolofliercreator/wp-content/themes/inkness/restore_controller.php
@@ -1,18 +1,24 @@
 <?php
+
 include_once("HomeMVC/Bolo_Model.php");
 
-$editmodel = new edit_model();
-$data = $editmodel->get_user_bolos();
-
-
-$model = new home_model();
-$view = new home_view();
-
-$agencies = $model->get_agencies();
-
-//get the bolos for the desired agency (default value = 'Show ALL')
-$result = $model->get_data($_POST['agency'], $_POST['offset']+0);
-//update the view with the new data
-$view->update_view($result, $data, $agencies, $_POST['agency'], $_POST['offset']+0);
+if($_POST['idBolo'] == "")
+{
+   echo("<h3>Sorry something wrong has occurred</h3>");
+}
+else
+{
+   $model = new Bolo_Model();
+   $result = $model->unarchive($_POST['idBolo']);
+   
+   if($result)
+   {
+     echo("<h3>BOLO restored successfully!");
+   }
+   else
+   {
+     echo("<h3>Some DATABASE error has occurred</h3>");
+   }
+}
 
 ?>
