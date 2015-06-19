ece7c2abf366f5af9e1840e262067bc0e39d1782
yflei001 <yflei001@fiu.edu>
2015-06-18 21:57:52 -0400

    Edit Agency functionality

--- Code/bolofliercreator/wp-content/themes/inkness/New Agency/new_agency_model.php
+++ Code/bolofliercreator/wp-content/themes/inkness/New Agency/new_agency_model.php
@@ -1,5 +1,4 @@
 <?php
-
 class agency_model{
 	
 	public function _construc(){
@@ -33,6 +32,33 @@ SQL;
 		return $result;
 	}//end 
 	
+	// EDIT AGENCY
+	public function update_agency($name, $address, $city, $zip, $phone){
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "bolo_creator";
+				
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = <<<SQL
+	    UPDATE `agencies` (name, st_address, city, zip, phone)
+		SET name = "$name", st_address = "$address", city = "$city", zip = "$zip", phone = "$phone"
+	   WHERE id = "$id"
+SQL;
+		if(!$result = $conn->query($sql)){
+		    die('There was an error running the query [' . $db->error . ']');
+		}
+		
+		mysqli_close($conn); 
+		return $result;
+	}//END EDIT AGENCY 
+	
 	public function get_agencies(){
 		$servername = "localhost";
 		$username = "root";
@@ -61,7 +87,4 @@ SQL;
 		
 	
 }//end class
-
-
-
 ?>
