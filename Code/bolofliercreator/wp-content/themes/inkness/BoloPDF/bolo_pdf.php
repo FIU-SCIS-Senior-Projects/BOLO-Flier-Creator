3fe5913a8f76bc8a1b208b4a6778a04ac8db2725
ZerolinK <david.e.vizcaino@gmail.com>
2015-07-23 01:18:32 -0400

    PDF of previous BOLO. WOrking implementation. Required a small change ot  acurrent system.

--- Code/bolofliercreator/wp-content/themes/inkness/BoloPDF/bolo_pdf.php
+++ Code/bolofliercreator/wp-content/themes/inkness/BoloPDF/bolo_pdf.php
@@ -1,67 +1,73 @@
 <?php
 
 class bolo_pdf{
-	
-	public function _construct(){
-		
-	}
-	
-	/**
-	 * creates a pdf document version of the bolo passed as a parameter.
-	 * the bolo fits in one single a4 size paper
-	 * the pdf document is downloaded automatically or prompted to open or save; depends on the browser being used 
-	 * 
-	 * @param $bolo this must be a mysqli_result object containing the bolo
-	 * you want to create a pdf of
+    
+    public function _construct(){
+        
+    }
+    
+    public function save_pdf($bolo, $display, $auth){
+    //include("mpdf60/mpdf.php");
+    //$bolo = $bolo->fetch_assoc();
+    $this->save($bolo->fetch_assoc(), $display, $auth);
+        
+        
+    }//end function save pdf
+    
+    /**
+     * creates a pdf document version of the bolo passed as a parameter.
+     * the bolo fits in one single a4 size paper
+     * the pdf document is downloaded automatically or prompted to open or save; depends on the browser being used 
+     * 
+     * @param $bolo this must be a mysqli_result object containing the bolo
+     * you want to create a pdf of
      * 
      * @param $display indicates whether to simply display the PDF or download it
-	 */
-	public function save_pdf($bolo, $display, $auth){
-		
-		include("mpdf60/mpdf.php");
-
-		$bolo_row = $bolo->fetch_assoc();
-		
-		//echo $ag_name = get_user_meta(get_current_user_id(), "agency", true);
-		$path="/bolofliercreator/wp-content/themes/inkness/".$bolo_row['image'];
-		$img = "http://bolo.cs.fiu.edu/bolofliercreator/wp-content/themes/inkness/".$bolo_row['image'];		       		
-				
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "bolo_creator";
-		// Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
-		if ($conn->connect_error) {
-		    die("Connection failed: " . $conn->connect_error);
-		}		
-				
-		$agency = $bolo_row['agency'];
-		$sql2 = <<<SQL
-		SELECT *
-		FROM agencies
-		WHERE name="$agency"
+     */
+    public function save($bolo, $display, $auth){
+        
+        include("mpdf60/mpdf.php");
+        $bolo = $bolo;
+        //echo $ag_name = get_user_meta(get_current_user_id(), "agency", true);
+        $path="/bolofliercreator/wp-content/themes/inkness/".$bolo['image'];
+        $img = "http://bolo.cs.fiu.edu/bolofliercreator/wp-content/themes/inkness/".$bolo['image'];                 
+                
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "bolo_creator";
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }       
+                
+        $agency = $bolo['agency'];
+        $sql2 = <<<SQL
+        SELECT *
+        FROM agencies
+        WHERE name="$agency"
 SQL;
-			
-		if(!$result = $conn->query($sql2)){
-		    die('There was an error running the query [' . $db->error . ']');
-		}
-		$row = $result->fetch_assoc();
-		
-		$id=$bolo_row['author'];
-		$sql3 = <<<SQL
-		SELECT display_name
-		FROM wp_users
-		WHERE ID="$id"
+            
+        if(!$result = $conn->query($sql2)){
+            die('There was an error running the query [' . $db->error . ']');
+        }
+        $row = $result->fetch_assoc();
+        
+        $id=$bolo['author'];
+        $sql3 = <<<SQL
+        SELECT display_name
+        FROM wp_users
+        WHERE ID="$id"
 SQL;
-			
-		if(!$result3 = $conn->query($sql3)){
-		    die('There was an error running the query [' . $db->error . ']');
-		}
-		$author = $result3->fetch_assoc();
-		
-		$sectionSQL = <<<SQL
+            
+        if(!$result3 = $conn->query($sql3)){
+            die('There was an error running the query [' . $db->error . ']');
+        }
+        $author = $result3->fetch_assoc();
+        
+        $sectionSQL = <<<SQL
         SELECT meta_value
         FROM wp_usermeta
         WHERE user_id="$id" AND meta_key="Section"
@@ -76,97 +82,97 @@ SQL;
 SQL;
         $rankResults = $conn->query($rankSQL);
         $rank = $rankResults->fetch_assoc();
-		
-		$html=    
-				
-		'<table style="width:100%; align=center">'.
-		'<tbody>'. 
-		   
-		'<tr>'.
-		'<td><img style="width:100;" src="'. $row['logo1'] .'"></td>'.
-		'<td></td>'.
-		'<td></td>'.
-		'<td></td>'.
-		'<td><p style="text-align:center; font-size:12px; text-transform:uppercase; color:red;">Unclassified// for official use only// law enforcement sensitive </p>
-		<h1 style="text-align:center; font-size:16px; margin-bottom:2px; font-weight: bold;">'. $row['name'].' Police Department</h1>
-		<h3 style="text-align:center; font-size:14px; line-height:1.2em; font-weight:300;"><em>'.$row['st_address'].'<br>'. $row['city'] .', FL, '. $row['zip'] .'<br>'. $row['phone'] .'</em></h3></td>'.
-		'<td><img style="width:100;" src="'. $row['logo2'] .'"></td>'.
-		'</tr>'.
-		
-		'</tbody>'.
-		'</table>'
-		
-		;
-		
-		$html2=
-				
-		'<h1 style="text-align:center; font-size:32px; text-transform:uppercase; color:red; font-weight: bold;">'.$bolo_row['selectcat'].'</h1>'.		
-		
-		'<table style="width:100%">'.
-		'<tbody>'.    
-		
-		'<tr>'.	
-		'<td rowspan="5"><img style="height: 200px" src="'.$img.'" attributes="#"\></td>'.
-		'</tr>'.
-		
-		'<tr>'.	
-		'<td></td>'.
-		'<td><p style="font-weight: bold;">Name: </p>'.$bolo_row ['myName'].' '.$bolo_row['lastName'].'</td>'.
-		'<td><p style="font-weight: bold;">D.O.B: </p>'.$bolo_row ['dob'].'</td>'.
-		'<td><p style="font-weight: bold;">License #: </p>'.$bolo_row ['license'].'</td>'.
-		'</tr>'.
-		
-		'<tr>'.
-		'<td></td>'.
-		'<td><p style="font-weight: bold;">Race: </p>'.$bolo_row ['race'].'</td>'.
-		'<td><p style="font-weight: bold;">Sex: </p>'.$bolo_row ['sex'].'</td>'.
-		'<td><p style="font-weight: bold;">Height: </p>'.$bolo_row ['height'].'</td>'.
-		'</tr>'.
-		
-		'<tr>'.
-		'<td></td>'.
-		'<td><p style="font-weight: bold;">Weight: </p>'.$bolo_row ['weight'].'</td>'.
-		'<td><p style="font-weight: bold;">Hair Color: </p>'.$bolo_row ['haircolor'].'</td>'.
-		'<td><p style="font-weight: bold;">Bolo ID: </td>'.
-		'</tr>'.
-		
-		'<tr>'.
-		'<td></td>'.
-		'<td><p style="font-weight: bold;">Address: </p>'.$bolo_row ['address'].'</td>'.
-		'<td><p style="font-weight: bold;">Tattoos/Scars: </p>'.$bolo_row ['tattoos'].'</td>'.
-		'<td></td>'.
-		'</tr>'.				
-		
-		'</tbody>'.
-		'</table>'		
-		;
-		
-		$html3=
-		'<br>'.
-		'Date Created: '. $bolo_row['datecreated'].
-		'<p style="font-size:14px; font-weight: bold;">Additional Info: </p>'.$bolo_row['adtnlinfo'].
-		
-		'<br><br>'.		
-		'<p style="font-size:14px; font-weight: bold;">Summary: </p>'.$bolo_row['summary']
-		;
-		
-		$html4=
-		
-		'<br><br>'.
-		'Any Agency having questions regarding this document may contact: ' . $section['meta_value'] . " " . $rank['meta_value'] . " " . $author['display_name']
+        
+        $html=    
+                
+        '<table style="width:100%; align=center">'.
+        '<tbody>'. 
+           
+        '<tr>'.
+        '<td><img style="width:100;" src="'. $row['logo1'] .'"></td>'.
+        '<td></td>'.
+        '<td></td>'.
+        '<td></td>'.
+        '<td><p style="text-align:center; font-size:12px; text-transform:uppercase; color:red;">Unclassified// for official use only// law enforcement sensitive </p>
+        <h1 style="text-align:center; font-size:16px; margin-bottom:2px; font-weight: bold;">'. $row['name'].' Police Department</h1>
+        <h3 style="text-align:center; font-size:14px; line-height:1.2em; font-weight:300;"><em>'.$row['st_address'].'<br>'. $row['city'] .', FL, '. $row['zip'] .'<br>'. $row['phone'] .'</em></h3></td>'.
+        '<td><img style="width:100;" src="'. $row['logo2'] .'"></td>'.
+        '</tr>'.
+        
+        '</tbody>'.
+        '</table>'
+        
+        ;
+        
+        $html2=
+                
+        '<h1 style="text-align:center; font-size:32px; text-transform:uppercase; color:red; font-weight: bold;">'.$bolo['selectcat'].'</h1>'.       
+        
+        '<table style="width:100%">'.
+        '<tbody>'.    
+        
+        '<tr>'. 
+        '<td rowspan="5"><img style="height: 200px" src="'.$img.'" attributes="#"\></td>'.
+        '</tr>'.
+        
+        '<tr>'. 
+        '<td></td>'.
+        '<td><p style="font-weight: bold;">Name: </p>'.$bolo ['myName'].' '.$bolo['lastName'].'</td>'.
+        '<td><p style="font-weight: bold;">D.O.B: </p>'.$bolo ['dob'].'</td>'.
+        '<td><p style="font-weight: bold;">License #: </p>'.$bolo ['license'].'</td>'.
+        '</tr>'.
+        
+        '<tr>'.
+        '<td></td>'.
+        '<td><p style="font-weight: bold;">Race: </p>'.$bolo ['race'].'</td>'.
+        '<td><p style="font-weight: bold;">Sex: </p>'.$bolo ['sex'].'</td>'.
+        '<td><p style="font-weight: bold;">Height: </p>'.$bolo ['height'].'</td>'.
+        '</tr>'.
+        
+        '<tr>'.
+        '<td></td>'.
+        '<td><p style="font-weight: bold;">Weight: </p>'.$bolo ['weight'].'</td>'.
+        '<td><p style="font-weight: bold;">Hair Color: </p>'.$bolo ['haircolor'].'</td>'.
+        '<td><p style="font-weight: bold;">Bolo ID: </td>'.
+        '</tr>'.
+        
+        '<tr>'.
+        '<td></td>'.
+        '<td><p style="font-weight: bold;">Address: </p>'.$bolo ['address'].'</td>'.
+        '<td><p style="font-weight: bold;">Tattoos/Scars: </p>'.$bolo ['tattoos'].'</td>'.
+        '<td></td>'.
+        '</tr>'.                
+        
+        '</tbody>'.
+        '</table>'      
+        ;
+        
+        $html3=
+        '<br>'.
+        'Date Created: '. $bolo['datecreated'].
+        '<p style="font-size:14px; font-weight: bold;">Additional Info: </p>'.$bolo['adtnlinfo'].
+        
+        '<br><br>'.     
+        '<p style="font-size:14px; font-weight: bold;">Summary: </p>'.$bolo['summary']
+        ;
+        
+        $html4=
+        
+        '<br><br>'.
+        'Any Agency having questions regarding this document may contact: ' . $section['meta_value'] . " " . $rank['meta_value'] . " " . $author['display_name']
      ;
-		
-		$mpdf=new mPDF('c','A4','','',15,15,15,15,15,15); 
+        
+        $mpdf=new mPDF('c','A4','','',15,15,15,15,15,15); 
 
-		$mpdf->SetDisplayMode('fullpage');
-		
-		$mpdf->list_indent_first_level = 0;	// 1 or 0 - whether to indent the first level of a list
-				
-		//$stylesheet = file_get_contents('mpdf60/examples/mpdfstyletables.css');
-		$mpdf->WriteHTML('mpdf60/examples/mpdfstyletables.css',1);
-		
-		$mpdf->WriteHTML($html.$html2.$html3.$html4,2);
-		
+        $mpdf->SetDisplayMode('fullpage');
+        
+        $mpdf->list_indent_first_level = 0; // 1 or 0 - whether to indent the first level of a list
+                
+        //$stylesheet = file_get_contents('mpdf60/examples/mpdfstyletables.css');
+        $mpdf->WriteHTML('mpdf60/examples/mpdfstyletables.css',1);
+        
+        $mpdf->WriteHTML($html.$html2.$html3.$html4,2);
+        
         if($display===TRUE)
         {
             if (realpath('uploads/preview' . $auth . '.pdf'))
@@ -182,9 +188,12 @@ SQL;
         
             echo $html.$html2;
         }
-		
-	}//end function save pdf
-	
-	
+        
+    }
+    
+    
+
+    
+    
 }//end class bolo_pdf
 ?>
--- Code/bolofliercreator/wp-content/themes/inkness/downloadBOLO.php
+++ Code/bolofliercreator/wp-content/themes/inkness/downloadBOLO.php
@@ -1,5 +1,4 @@
 <?php
-
     include_once'flier_model.php';
     $flier = new FlierModel();
 
@@ -7,47 +6,11 @@
     $doc = new bolo_pdf();
 
     $boloID = $_GET["id"];
-    //grab BOLO data from database NOTE: this is a copy of code from BoloSelected.php
+    echo("BoloID is " . $boloID);
+    //grab BOLO data from database
     include_once('Model_Bolo.php');
     $bolo = new Model_Bolo();
-    $resultProp1= $bolo->infoBolo($prop1);
-    $picture = $bolo->loadPicture();
 
-
-    while ($row= mysqli_fetch_array($resultProp1))
-    {
-
-        $resultId =  $row['bolo_id'];
-        $resultAddr = $row['address'];
-        $resultAdt = $row['adtnlinfo'];
-        $resultDob = $row['dob'];
-        $resultHair = $row['haircolor'];
-        $resultHeight = $row['height'];
-        $pathImage = $row['image'];
-        $resultName = $row['myName'];
-        $resultlastName = $row['lastName'];
-        $resultRace = $row['race'];
-        $resultCat = $row['selectcat'];
-        $resultSex = $row['sex'];
-        $resultSumm = $row['summary'];
-        $resultTattoo = $row['tattoos'];
-        $resultWeight = $row['weight'];
-        $resultLicense = $row['license'];
-        $resultDate = $row['datecreated'];
-        $author = $row['author'];
-        $update = $row['update_status'];
-        $link = $row['link'];
-        $resultAgency = $row['agency'];
-    }
-    
-    $author_name = $bolo->loadAuthor($author);
-    
-    
-    
-    //check to see if a PDF has been made of this BOLO before...
-    /*note: there is no real primary key for this table, so getting a BOLO wil lrequire extensive query or a rebuild of the entire table with a unique primary key (BoloID is not a viable primary key)
-      and neither is dateCeated since a pdf may have been created before the actual BOLO being made. This leaves this hacked together query that does NOT guarantee uniqueness*/
-    
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -56,33 +19,64 @@
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     
-    $boloPDFsql = <<<SQL
-    SELECT *
-    FROM `bolo_pdf`
-    WHERE selectcat='$resultCat' , myName='$resultName' , lastName=' $resultlastName', dob='$resultDob', license='$resultLicense', race='$resultRace', sex='$resultSex', 
-        height='$resultHeight', weight='$resultWeight', haircolor='$resultHair',address='$resultAddr',tattoos='$resultTattoo',summary='$resultSumm',
-        adtnlinfo='$resultAdt',author='$author',agency='$resultAgency',image='$pathImage',link='$link'"; 
+    $dataQuery = <<<SQL
+        SELECT *
+        FROM `wp_flierform`
+        WHERE bolo_id='$boloID';
 SQL;
-       
-    $previousPDF = $conn->query($boloPDFsql);
+    $boloData = $conn->query($dataQuery);
+    
+    $row= $boloData->fetch_assoc();
+
+    $resultId =  $row['bolo_id'];
+    $resultAddr = $row['address'];
+    $resultAdt = $row['adtnlinfo'];
+    $resultDob = $row['dob'];
+    $resultHair = $row['haircolor'];
+    $resultHeight = $row['height'];
+    $pathImage = $row['image'];
+    $resultName = $row['myName'];
+    $resultlastName = $row['lastName'];
+    $resultRace = $row['race'];
+    $resultCat = $row['selectcat'];
+    $resultSex = $row['sex'];
+    $resultSumm = $row['summary'];
+    $resultTattoo = $row['tattoos'];
+    $resultWeight = $row['weight'];
+    $resultLicense = $row['license'];
+    $resultDate = $row['datecreated'];
+    $author = $row['author'];
+    $update = $row['update_status'];
+    $link = $row['link'];
+    $resultAgency = $row['agency'];
+    
+    
+    echo("BOLO ID is ".$resultId ." and image is " . $row['image']);
+    
+    
+    //check to see if a PDF has been made of this BOLO before...
+    /*note: there is no real primary key for this table, so getting a BOLO wil lrequire extensive query or a rebuild of the entire table with a unique primary key (BoloID is not a viable primary key)
+      and neither is dateCeated since a pdf may have been created before the actual BOLO being made. This leaves this hacked together query that does NOT guarantee uniqueness*/
+    
+    
+    
+    $boloPDFpreview = "SELECT * FROM bolo_pdf WHERE selectcat='".$resultCat."', myName='".$resultName."', lastName='".$resultlastName."', dob='".$resultDob."', license='".$resultLicense."', race='".$resultRace."', sex='".$resultSex."', height='".$resultHeight."', weight='".$resultWeight."', haircolor='".$resultHair."',address='".$resultAddr."',tattoos='".$resultTattoo."',summary='".$resultSumm."',adtnlinfo='".$resultAdt."',author='".$author."',agency='".$resultAgency."',image='".$pathImage."',link='".$link."'";
+    
+    $queryCheck = mysqli_query($conn, "SELECT * FROM bolo_pdf WHERE selectcat='".$resultCat."', myName='" . $resultName . "', lastName='" . $resultlastName . "', dob='" . $resultDob . "', license='". $resultLicense. "', race='" . $resultRace . "', sex='" . $resultSex . "', height='" . $resultHeight . "', weight='" . $resultWeight . "', haircolor='" . $resultHair . "',address='" . $resultAddr . "',tattoos='" . $resultTattoo . "',summary='" . $resultSumm . "', adtnlinfo='" . $resultAdt . "',author='" . $author . "',agency='" . $resultAgency . "',image='" . $pathImage . "',link='" . $link . "'");
+    
     
      //actually check if there was a result and if there was...
-     if(mysqli_num_rows($previousPDF) > 0){
 
-        //use the data to make the pdf
-        $doc->save_pdf($previousPDF, FALSE, $author);
-        
-    }else{
-        
         //if not, add it to the database (not, DB has no primary key...)
-        $flier->submit_pdf($resultCat, $resultName, $resultlastName, $resultDob, $resultLicense, $resultRace, $resultSex, $resultHeight, $resultWeight,
-          $resultHair, $resultAddr, $resultTattoo, $resultSumm, $resultAdt, $pathImage, $author, $resultAgency, $link);    
+        //$flier->submit_pdf($resultCat, $resultName, $resultlastName, $resultDob, $resultLicense, $resultRace, $resultSex, $resultHeight, $resultWeight,
+        //  $resultHair, $resultAddr, $resultTattoo, $resultSumm, $resultAdt, $pathImage, $author, $resultAgency, $link);    
         //since it was freshly added, retrieve it and send it to the pdf creator
-        $result = $flier->get_bolo();
-        $doc->save_pdf($result, FALSE, $author);
+        //$result = $flier->get_bolo();
+        $doc->save($row, FALSE, $author);
         
-    }
+    
      
+     mysqli_close($conn);
     
     
 ?>
