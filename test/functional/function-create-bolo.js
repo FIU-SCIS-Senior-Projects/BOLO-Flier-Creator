9b5ae76e804ee4c77b31848bf99a1286ca79c203
Ryan del Rosario <rdelr011@fiu.edu>
2015-09-30 15:29:13 -0400

    Minor Refactoring of Create BOLO Feature Test
    * Modify jshint options for better linting
    * Add 'use strict' instruction
    * Fix paths for multiplatform (Windows, *nux, OSX)
    * Minor refactoring of test specification

--- test/functional/function-create-bolo.js
+++ test/functional/function-create-bolo.js
@@ -1,41 +1,42 @@
-/* jshint expr:true */
-/* global describe, it */
-
+/* jshint node: true, mocha: true, expr:true */
+'use strict';
 
 /*
  * Feature Test: Create BOLO
  *
  */
 
-
 /* Dependencies */
 var expect = require('chai').expect;
 var path = require('path');
 
-var src_dir = path.join(__dirname, '../../src/');
+var src_dir = path.resolve( __dirname, '../../src/' );
 
-var ClientAccess = require(src_dir + 'core/ports/client-access-port.js');
-var AdapterFactory = require(src_dir + 'core/adapters');
+var ClientAccessPort = require( path.join( src_dir, 'core/ports/client-access-port.js' ) );
+var AdapterFactory = require( path.join( src_dir, 'core/adapters' ) );
 
 
 /* Feature Test Specification */
-describe('Officer creates a BOLO', function () {
+describe( 'Officer creates a BOLO', function () {
+    var clientAccess;
+    var adapterFactory, storageAdapter;
 
-    it('should save the BOLO to the system', function () {
-        var clientAccess = new ClientAccess();
-        var storageAdapter = AdapterFactory.create('storage', 'mock');
+    beforeEach( function () {
+        clientAccess = new ClientAccessPort();
+        storageAdapter = AdapterFactory.create( 'storage', 'mock' );
+    });
+
+    it( 'should save the BOLO to the system', function () {
         var date = new Date();
+        var response;
 
         // given the BOLO has sufficient information
         var formData = {
             creationDate : date,
-            lastUpdate : date,
             authorFName : "Jason",
             authorLName : "Cohen",
             authorUName : "jcohen",
             category : "ROBBERY",
-            imageURL : [],
-            videoLink : [],
             firstName : 'Barry',
             lastName : 'Badman',
             dob : "01-23-1945",
@@ -54,10 +55,10 @@ describe('Officer creates a BOLO', function () {
         };
 
         // when the Officer submits the BOLO to be saved
-        var response = clientAccess.createBolo(formData, storageAdapter);
+        response = clientAccess.createBolo( formData, storageAdapter );
 
         // then the system will return a success message
-        expect(response.success).to.be.true;
+        expect( response.success ).to.be.true;
     });
 
 });
