208e4760db84f7dc85474387e26222ef51aaec9c
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-02 23:20:37 -0400

    Update Functional Create BOLO Test
    Tests now use the the newest ClientAccessPort interface along with using
    the FileMediaAdapter module for storing attachments.

--- test/functional/function-create-bolo.js
+++ test/functional/function-create-bolo.js
@@ -6,59 +6,84 @@
  *
  */
 
-/* Dependencies */
+/* Testing Utilities */
 var expect = require('chai').expect;
-var path = require('path');
 
+
+/* Dependencies */
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var Promise = require('promise');
+var FileFactory = require( '../lib/file-fixture-factory.js' );
+
+
+/* Base Project Path */
 var src_dir = path.resolve( __dirname, '../../src/' );
 
-var ClientAccessPort = require( path.join( src_dir, 'core/ports/client-access-port.js' ) );
+var ClientAccess = require( path.join( src_dir, 'core/ports/client-access-port.js' ) );
 var AdapterFactory = require( path.join( src_dir, 'core/adapters' ) );
 
 
-/* Feature Test Specification */
-describe( 'Officer creates a BOLO', function () {
-    var clientAccess;
-    var adapterFactory, storageAdapter;
+/* Test Fixtures */
+var formData = {
+    creationDate    : ( new Date() ).toString(),
+    lastUpdate      : '',
+    agency          : 'Pinecrest',
+    authorFName     : 'Jason',
+    authorLName     : 'Cohen',
+    authorUName     : 'jcohen',
+    category        : 'ROBBERY',
+    firstName       : 'Barry',
+    lastName        : 'Badman',
+    dob             : '01-23-1945',
+    dlNumber        : 'D123-456-78-900-0',
+    race            : 'Asian',
+    sex             : 'M',
+    height          : '6-01',
+    weight          : '185',
+    hairColor       : 'Black',
+    tattoos         : 'Tomato tattoo on right chest',
+    address         : '123 Gangsta Lane',
+    additional      : '',
+    summary         : '',
+    archive         : false
+};
 
-    beforeEach( function () {
-        clientAccess = new ClientAccessPort();
+
+/* Feature Test Specification */
+describe( 'When the Officer creates a BOLO', function () {
+    var clientAccess;
+    var adapterFactory, storageAdapter, mediaAdapter;
+    var imageFactory, imageAtachment;
+
+    before( function () {
         storageAdapter = AdapterFactory.create( 'storage', 'mock' );
+        mediaAdapter = AdapterFactory.create( 'media', 'file' );
+        process.env.FILE_STORAGE_PATH = os.tmpdir();
+        imageFactory = new FileFactory( path.join( __dirname, '../assets/nodejs.png' ) );
     });
 
-    it( 'should save the BOLO to the system', function () {
-        var date = new Date();
-        var response;
+    beforeEach( function () {
+        clientAccess = new ClientAccess( storageAdapter, mediaAdapter );
+    });
 
-        // given the BOLO has sufficient information
-        var formData = {
-            creationDate : date,
-            authorFName : "Jason",
-            authorLName : "Cohen",
-            authorUName : "jcohen",
-            category : "ROBBERY",
-            firstName : 'Barry',
-            lastName : 'Badman',
-            dob : "01-23-1945",
-            dlNumber : 'D123-456-78-900-0',
-            race : "Asian",
-            sex : "M",
-            height : "6-01",
-            weight : "185",
-            hairColor : "Black",
-            tattoos : "Tomato tattoo on right chest",
-            address : "123 Gangsta Lane",
-            additional : "",
-            summary : "",
-            agency : "Pinecrest",
-            archive : false
-        };
+    it( 'the BOLO is saved to the system', function () {
+        var imagePromise = imageFactory.create( 'file1' );
+        var savePromise;
 
         // when the Officer submits the BOLO to be saved
-        response = clientAccess.createBolo( formData, storageAdapter );
+        savePromise = imagePromise
+            .then( function ( image ) {
+                var attach = { image: [ image ] };
+                return clientAccess.createBolo( formData, attach );
+            });
 
         // then the system will return a success message
-        expect( response.success ).to.be.true;
+        return savePromise
+            .then( function ( response ) {
+                expect( response.success ).to.be.true;
+            });
     });
 
 });
