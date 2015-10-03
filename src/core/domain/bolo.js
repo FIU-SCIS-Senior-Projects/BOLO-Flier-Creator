cbaff997dec6fd8b81df9edde3f3da63c678c1e5
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-02 21:12:09 -0400

    Implement Crude Bolo Validation
    The validation uses an object with expected keys whose values are an
    object defining its type and requirement status. The strategy is an
    initial naive validation implementation and should be looked into before
    deploying to production.

--- src/core/domain/bolo.js
+++ src/core/domain/bolo.js
@@ -6,6 +6,18 @@
  *
  */
 
+var schema = {
+    authorFName     : { required: true, type: 'string' },
+    authorLName     : { required: true, type: 'string' },
+    authorUName     : { required: true, type: 'string' },
+    agency          : { required: true, type: 'string' }
+};
+
+var required = Object.keys( schema ).filter( function ( key ) {
+    return schema[key].required;
+});
+
+
 /**
  * Bolo constructor
  *
@@ -21,8 +33,16 @@ function Bolo ( data ) {
  * Checks if the bolo is valid
  */
 Bolo.prototype.isValid = function () {
-    // TODO Validate data
-    return true;
+    // TODO Naive validation implementation, refactor using a robust validation
+    // library like Joi. It might be useful to implement validation with a
+    // Bolo Template object
+    var data = this.data;
+    var result = required.reduce( function ( reqs, key ) {
+        if ( data[key] && typeof data[key] === schema[key].type )
+            return reqs.concat( key );
+    }, [] ) || [];
+    // if all required keys were pushed, then valid
+    return ( result.length === required.length );
 };
 
 module.exports = Bolo;
