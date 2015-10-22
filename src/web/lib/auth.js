185355ce3797ed3cb7dcffcf2a68c2b4f404c9fe
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-22 16:16:34 -0400

    Authenticate using the application UserServicePort
    The user-service-port provides an interface for client applications to
    authenticate users stored in the core domain application.

--- src/web/lib/auth.js
+++ src/web/lib/auth.js
@@ -3,31 +3,44 @@
 
 var bodyParser = require('body-parser');
 var csrf = require('csurf');
-var router = require('express').Router();
+var path = require('path');
 var passport = require('passport');
+var router = require('express').Router();
 var LocalStrategy = require('passport-local').Strategy;
 
+var core = path.resolve( __dirname, '../../core' );
+var UserService = require( path.join( core, 'ports/user-service-port' ) );
+var AdapterFactory = require( path.join( core, 'adapters' ) );
 
 var _csrf = csrf({ 'cookie': true });
 var _bodyparser = bodyParser.urlencoded({ 'extended': true });
 
+var userRepository = AdapterFactory.create( 'storage', 'cloudant-user' );
+var userService = new UserService( userRepository );
 
 passport.use( new LocalStrategy(
     function ( username, password, done ) {
-        if ( username != 'superuser' && password != 'superuser' ) {
-            return done( null, false, { 'message': 'Invalid credentials.' } );
-        }
+        userService.authenticate( username, password )
+        .then( function( account ) {
+            if ( !account ) {
+                var msg = 'Invalid credentials';
+                return done( null, false, { 'message': msg } );
+            }
 
-        return done( null, { 'id': 'superuser' } );
+            return done( null, account );
+        });
     }
 ));
 
 passport.serializeUser( function ( user, done ) {
-    done( null, user.id );
+    done( null, user.data.id );
 });
 
 passport.deserializeUser( function ( id, done ) {
-    done( null, { 'id': id } );
+    userService.deserializeId( id )
+    .then( function ( account ) {
+        if ( account ) done( null, account );
+    });
 });
 
 
