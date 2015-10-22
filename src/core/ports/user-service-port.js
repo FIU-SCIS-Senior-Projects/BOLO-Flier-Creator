3934299bd63ecc4935edcb230a2347cd8e20522c
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-22 16:06:21 -0400

    Add jsdoc comments to user-service-port.js

--- src/core/ports/user-service-port.js
+++ src/core/ports/user-service-port.js
@@ -4,13 +4,33 @@
 var Promise = require('promise');
 
 
+/** @module core/ports */
 module.exports = UserServicePort;
 
 
+/**
+ * Creates a new instance of {UserServicePort}.
+ *
+ * @class
+ * @classdesc Provides an API for interacting with application user
+ * interfaces.
+ *
+ * @param {StrageAdapter|UserRepository} - Oobject implementing the User
+ * Repository Storage Port Interface.
+ */
 function UserServicePort ( userRepository ) {
     this.userRepository = userRepository;
 }
 
+/**
+ * Authenticate a username and password pair.
+ *
+ * @param {String} - the username to authenticate
+ * @param {String} - the password to authenticate for the supplied username
+ *
+ * @returns {Promise|User} the User object matching the supplied username and
+ * password. Null if authentication fails.
+ */
 UserServicePort.prototype.authenticate = function ( username, password ) {
     var userPromise = this.userRepository.getByUsername( username );
 
@@ -24,6 +44,13 @@ UserServicePort.prototype.authenticate = function ( username, password ) {
     });
 };
 
+/**
+ * Deserialize a user id.
+ *
+ * @param {String} - the User's id to deserialize
+ *
+ * @returns {Promise|User} the User object matching the supplied id.
+ */
 UserServicePort.prototype.deserializeId = function ( id ) {
     return this.userRepository.getById( id );
 };
