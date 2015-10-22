327846e34a0ff57b5e2355aa781bab2c952a79f8
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-22 13:33:15 -0400

    Revise account service port tests
    The User repository mock should follow the Promises pattern.

--- test/unit/account-service-port-test.js
+++ test/unit/account-service-port-test.js
@@ -5,6 +5,7 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 
 var path = require('path');
+var Promise = require('promise');
 
 var src = path.resolve( __dirname, '../../src' );
 var AccountService = require( path.join( src, 'core/ports/account-service-port' ) );
@@ -15,26 +16,32 @@ describe( 'account service port', function () {
 
     var accountService;
     var mockUserRepo;
-    var user;
 
     beforeEach( function () {
-        user = UserFixture.create();
         mockUserRepo = {};
         accountService = new AccountService( mockUserRepo );
     });
 
-    it( 'authenticates the validity of user credentials', function () {
-        /* arrange */
-        var stub = mockUserRepo.getByUsername = sinon.stub();
-        stub.withArgs( sinon.match.string ).returns( user );
-        var username = 'superhacker',
-            password = 'hackersuper';
+    describe( 'authenticates user credentials', function () {
+        it( 'promises a User object for valid credentials', function () {
+            /* arrange */
+            var user        = UserFixture.create();
+            var username    = 'superhacker',
+                password    = 'hackersuper';
 
-        /* act */
-        var response = accountService.authenticate( username, password );
+            mockUserRepo.getByUsername = sinon.stub()
+                .withArgs( sinon.match.string )
+                .returns( Promise.resolve( user ) );
 
-        /* assert */
-        expect( response ).to.equal( user );
-    });
+            /* act */
+            var response = accountService.authenticate( username, password );
+
+            /* assert */
+            expect( response ).to.equal( user );
+        });
+
+        it.skip( 'promises a null for invalid credentials', function () {
+        });
+    }); /* end describe: authenticates user credentials */
 
 });
