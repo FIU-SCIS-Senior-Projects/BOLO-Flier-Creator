b68323be1425e4e440337bc6df41bb6229b56288
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-26 18:19:51 -0400

    Refactor insert for attachments

--- src/core/adapters/persistence/cloudant-bolo-repository.js
+++ src/core/adapters/persistence/cloudant-bolo-repository.js
@@ -2,7 +2,9 @@
 'use strict';
 
 var _ = require('lodash');
+var fs = require('fs');
 var Promise = require('promise');
+var uuid = require('node-uuid');
 
 var db = require('../../lib/cloudant-promise').db.use('bolo');
 var Bolo = require('../../domain/bolo.js');
@@ -91,12 +93,28 @@ function CloudantBoloRepository () {
  * @param {Object} - Data to store
  */
 CloudantBoloRepository.prototype.insert = function ( bolo, attachments ) {
-    if ( attachments ) {
-        return insertBoloWithAttachments( bolo, attachments );
-    } else {
-        return insertBolo( bolo );
-    }
+    var newdoc = boloToCloudant( bolo );
+    newdoc._id = newdoc.agency + uuid.v4().toString();
+
+    return Promise.all( attachments.map( transformAttachment ) )
+        .then( function ( attDTOs ) {
+            if ( attDTOs.length ) {
+                return db.insertMultipart( newdoc, attDTOs, newdoc._id );
+            } else {
+                return db.insert( newdoc );
+            }
+        })
+        .then( function ( response ) {
+            if ( !response.ok ) throw new Error( response.error );
+            newdoc._id = response.id;
+            return Promise.resolve( boloFromCloudant( newdoc ) );
+        })
+        .catch( function ( error ) {
+            console.error( 'Cloudant insert error: ', error );
+            throw new Error( 'Cloudant encountered an error inserting a document' );
+        });
 };
+
 
 /**
  * Updata a BOLO in the BOLO respository
@@ -160,70 +178,9 @@ CloudantBoloRepository.prototype.getBolo = function (id) {
 };
 
 
-
 /*
+ *
  * Helper Methods
+ *
  */
-
-function insertBolo ( bolo ) {
-    var newbolo = new Bolo( bolo.data );
-    newbolo.data.Type = DOCTYPE;
-
-    return db.insert( newbolo.data )
-        .then( function ( response ) {
-            if ( !response.ok ) throw new Error( 'Unable to add BOLO' );
-
-            delete newbolo.data.Type;
-            newbolo.data.id = response.id;
-
-            return Promise.resolve( newbolo );
-        })
-        .catch( function ( error ) {
-            return Promise.reject( error );
-        });
-}
-
-function insertBoloWithAttachments ( bolo, attachments ) {
-    var newbolo = new Bolo( bolo.data );
-    newbolo.data.Type = DOCTYPE;
-
-    return db.insert( newbolo.data )
-        .then ( function ( response ) {
-            if ( !response.ok ) throw new Error( 'Unable to add BOLO' );
-
-            delete newbolo.data.Type;
-            newbolo.data.id = response.id;
-
-            var promises = [];
-
-            attachments.forEach( function ( att ) {
-                promises.push( insertAttachment( response.id, att ) );
-            });
-
-            return Promise.all( promises );
-        })
-        .then( function () {
-            return Promise.resolve( newbolo );
-        })
-        .catch( function ( error ) {
-            return Promise.reject( error );
-        });
-}
-
-function insertAttachment ( docname, attachment ) {
-    attachment.data.on( 'end', function () {
-        return Promise.resolve( true );
-    });
-
-    attachment.data.pipe(
-        db.db.attachment.insert(
-            docname,
-            attachment.filename,
-            null,
-            attachment.content_type
-        )
-    );
-
-    attachment.data.resume();
-}
 
