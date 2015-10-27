3a51ba9688c4d1c99e74be0d87a1af0123f84341
Ryan del Rosario <rdelr011@fiu.edu>
2015-10-27 00:38:50 -0400

    Update cloudant-bolo-repository integration test

--- test/integration/cloudant-bolo-repository-test.js
+++ test/integration/cloudant-bolo-repository-test.js
@@ -10,11 +10,13 @@ var src = path.resolve( __dirname, '../../src' );
 var BoloFixture = require('../lib/bolo-entity-fixture');
 var AdapterFactory = require( path.join( src, 'core/adapters') );
 
-require('dotenv').config({ path: path.resolve( '../../.env' ) });
+require('dotenv').config({ path: path.resolve( __dirname, '../../.env' ) });
 
 describe( 'BOLO Repository Storage Adapter', function () {
-    var bolo, insertedBoloID;
     var boloRepository;
+    var bolo, insertedBoloID;
+
+    this.timeout( 5000 );
 
     before( function () {
         boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
@@ -24,12 +26,12 @@ describe( 'BOLO Repository Storage Adapter', function () {
         bolo = BoloFixture.create();
     });
 
-    it( 'promises to #add a bolo into the repository', function () {
+    it( 'promises to #insert a bolo into the repository', function () {
         /* act */
         var boloPromise = boloRepository.insert( bolo );
 
         /* assert */
-        return boloPromise 
+        return boloPromise
             .then( function ( newbolo ) {
                 expect( bolo.diff( newbolo ) ).to.be.length( 1 )
                     .and.to.contain( 'id' );
@@ -38,15 +40,31 @@ describe( 'BOLO Repository Storage Adapter', function () {
     });
 
 
-    it( 'promises to #delete a bolo from the repository', function () {
-        /* act */
-        var responsePromise = boloRepository.delete( insertedBoloID );
+    describe( '#delete method', function () {
+        it( 'promises an error if the bolo id is not found', function () {
+            /* act */
+            var responsePromise = boloRepository.delete( 'non-existant-id' );
 
-        /* assert */
-        return responsePromise
-            .then( function ( response ) {
-                expect( response.ok ).to.be.true;
-                expect( response.id ).to.be.equal( insertedBoloID );
-            });
+            /* assert */
+            return responsePromise
+                .then( function ( response ) {
+                    // test failure
+                })
+                .catch( function ( response ) {
+                    expect( response ).to.be.instanceOf( Error );
+                });
+        });
+
+        it( 'promises the id and ok status', function () {
+            /* act */
+            var responsePromise = boloRepository.delete( insertedBoloID );
+
+            /* assert */
+            return responsePromise
+                .then( function ( response ) {
+                    expect( response.ok ).to.be.true;
+                    expect( response.id ).to.be.equal( );
+                });
+        });
     });
 });
