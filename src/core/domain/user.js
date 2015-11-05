ab2c8db337daaca576d90fd78a190ce4c15ac612
Ryan del Rosario <rdelr011@fiu.edu>
2015-11-05 18:08:32 -0500

    Add naive validation to user entity

--- src/core/domain/user.js
+++ src/core/domain/user.js
@@ -3,6 +3,25 @@
 
 var _ = require('lodash');
 
+var schema = {
+    'username': {
+        'required'  : true,
+        'type'      : 'string'
+    },
+    'email': {
+        'required'  : true,
+        'type'      : 'string'
+    },
+    'password': {
+        'required'  : true,
+        'type'      : 'string'
+    },
+    'tier': {
+        'required'  : true,
+        'type'      : 'number'
+    }
+};
+
 var userTemplate = {
     'id'            : null,
     'username'      : null,
@@ -16,6 +35,10 @@ var userTemplate = {
     'sectunit'      : null,
     'ranktitle'     : null
 };
+
+var required = Object.keys( schema ).filter( function ( key ) {
+    return schema[key].required;
+});
 
 /** @module core/domain */
 module.exports = User;
@@ -31,6 +54,25 @@ module.exports = User;
 function User ( data ) {
     this.data = _.extend( {}, userTemplate, data );
 }
+
+/**
+ * Ensure the consistency of user data.
+ *
+ * @returns {bool} true if passes validation, false oherwise
+ *
+ * @todo Refactor the validation mechanism. This approach is a naive
+ * implementation and should be reviewed. The domain/bolo.js module uses a
+ * similar approach that shoud be refactored as well.
+ */
+User.prototype.isValid = function () {
+    var data = this.data;
+    var result = required.filter( function ( key ) {
+        if ( data[key] && typeof data[key] === schema[key].type )
+            return data[key];
+    }, []) || [];
+
+    return ( result.length === required.length );
+};
 
 /**
  * Check if the supplied user object has the same attributes.
