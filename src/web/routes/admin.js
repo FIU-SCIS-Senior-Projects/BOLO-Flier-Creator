ea8cb77e331bddf2923bb942b466a30f29d50756
Ryan del Rosario <rdelr011@fiu.edu>
2015-11-11 15:07:59 -0500

    Implement form processing for user registration

--- src/web/routes/admin.js
+++ src/web/routes/admin.js
@@ -11,6 +11,9 @@ var core = path.resolve( __dirname, '../../core/' );
 var UserService = require( path.join( core, 'service/user-service' ) );
 var AdapterFactory = require( path.join( core, 'adapters' ) );
 
+var userRepository = AdapterFactory.create( 'persistence', 'cloudant-user-repository' );
+var userService = new UserService( userRepository );
+
 module.exports = router;
 
 /** @todo Extract into a common library */
@@ -22,17 +25,15 @@ function cleanTemporaryFiles ( files ) {
 
 function setUserData ( fields ) {
     return {
-        'id'            : fields.id || '',
         'username'      : fields.username || '',
         'email'         : fields.email || '',
         'fname'         : fields.fname || '',
         'lname'         : fields.lname || '',
-        'password'      : fields.password || '',
-        'tier'          : 1,
+        'password'      : fields.password,
+        'tier'          : fields.role || null,
         'badge'         : fields.badge || '',
         'secunit'       : fields.secunit || '',
         'ranktitle'     : fields.ranktitle || ''
-        /** @todo 'tier' needs to be handled somewhere */
     };
 }
 
@@ -72,29 +73,38 @@ router.get( '/', function ( req, res ) {
  * Responds with a form to create a new user.
  */
 router.get( '/createuser', function ( req, res ) {
-    res.render( 'create-user-form' );
+    res.render( 'create-user-form', {
+        'roles': userService.getRoleNames()
+    });
 });
-
 
 /**
  * POST /createuser
+ * Process data to create a user, respond with the result.
  */
-router.post( '/createuser',function ( req, res ) {
-    var userRepository = AdapterFactory.create( 'persistence', 'cloudant-user-repository' );
-    var userService = new UserService( userRepository );
+router.post( '/createuser', function ( req, res ) {
+    var userRoles = userService.getRoleNames();
+
     parseFormData( req )
         .then( function ( formDTO ) {
             var userDTO = setUserData( formDTO.fields );
+            if ( userDTO.tier ) {
+                userDTO.tier = userService.getRole( userDTO.tier );
+            }
             var result = userService.registerUser( userDTO, formDTO.files );
             return Promise.all([ result, formDTO ]);
         })
         .then( function ( pData ) {
-            if( pData[1].files.length ) cleanTemporaryFiles( pData[1].files );
-            res.render( 'create-user-form', { 'msg': 'Successfully registered user.' } );
+            if ( pData[1].files.length ) cleanTemporaryFiles( pData[1].files );
+            res.render( 'create-user-form', {
+                'msg': 'Successfully registered user.',
+                'roles': userRoles
+            });
         })
         .catch( function ( error ) {
-            /** @todo send back form data with error message */
-            console.error( '>>> register user route error: ', error );
-            res.render( 'create-user-form', { 'error': error } );
+            res.render( 'create-user-form', {
+                'error': error,
+                'roles': userRoles
+            });
         });
 });
