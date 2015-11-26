286dff6c3857e89aa1d06396ac53d090223994a5
Rodney Sanchez <rsanc172@fiu.edu>
2015-11-26 13:33:15 -0500

    Added logic for the pagination

--- src/web/routes/bolos.js
+++ src/web/routes/bolos.js
@@ -8,9 +8,9 @@ var path = require('path');
 var Promise = require('promise');
 var router = require('express').Router();
 
-var core_dir = path.resolve( __dirname + '../../../core/' );
-var BoloService = require( path.join( core_dir, 'service/bolo-service') );
-var AdapterFactory = require( path.join( core_dir, 'adapters' ) );
+var core_dir = path.resolve(__dirname + '../../../core/');
+var BoloService = require(path.join(core_dir, 'service/bolo-service'));
+var AdapterFactory = require(path.join(core_dir, 'adapters'));
 
 //gets current time; useful for bolo creation and update
 function getDateTime() {
@@ -34,47 +34,47 @@ function getDateTime() {
 
 function setBoloData(fields) {
     return {
-        id                  : fields.id || '',
-        createdOn           : fields.enteredDT ? fields.enteredDT : getDateTime(),
-        lastUpdatedOn       : fields.lastUpdatedOn ? fields.lastUpdatedOn : getDateTime(),
-        agency              : "Pinecrest Police Department",
-        authorFName         : "Jason",
-        authorLName         : "Cohen",
-        authorUName         : "Jason Cohen",
-        category            : fields.bolo_category != 'Select an option...' ? fields.bolo_category : '',
-        firstName           : fields.fname || '',
-        lastName            : fields.lname || '',
-        dob                 : fields.dob || '',
-        dlNumber            : fields.dl_number || '',
-        race                : fields.race || '',
-        sex                 : fields.sex != 'Select an option...' ? fields.sex : '',
-        height              : fields.height || '',
-        weight              : fields.weight || '',
-        hairColor           : fields.hair_color != 'Select an option...' ? fields.hair_color : '',
-        tattoos             : fields.tattoos || '',
-        address             : fields.address || '',
-        additional          : fields.last_known_address || '',
-        summary             : fields.summary || '',
-        attachments         : {},
-        video_url           : fields.video_url || '',
-        isActive            : fields.isActive || true,
-        status              : fields.status || "New"
+        id: fields.id || '',
+        createdOn: fields.enteredDT ? fields.enteredDT : getDateTime(),
+        lastUpdatedOn: fields.lastUpdatedOn ? fields.lastUpdatedOn : getDateTime(),
+        agency: "Pinecrest Police Department",
+        authorFName: "Jason",
+        authorLName: "Cohen",
+        authorUName: "Jason Cohen",
+        category: fields.bolo_category != 'Select an option...' ? fields.bolo_category : '',
+        firstName: fields.fname || '',
+        lastName: fields.lname || '',
+        dob: fields.dob || '',
+        dlNumber: fields.dl_number || '',
+        race: fields.race || '',
+        sex: fields.sex != 'Select an option...' ? fields.sex : '',
+        height: fields.height || '',
+        weight: fields.weight || '',
+        hairColor: fields.hair_color != 'Select an option...' ? fields.hair_color : '',
+        tattoos: fields.tattoos || '',
+        address: fields.address || '',
+        additional: fields.last_known_address || '',
+        summary: fields.summary || '',
+        attachments: {},
+        video_url: fields.video_url || '',
+        isActive: fields.isActive || true,
+        status: fields.status || "New"
     };
 }
 
-function parseFormData ( req ) {
-    return new Promise( function ( resolve, reject ) {
+function parseFormData(req) {
+    return new Promise(function (resolve, reject) {
         var form = new multiparty.Form();
         var files = [];
         var fields = {};
         var result = { 'files': files, 'fields': fields };
 
-        form.on( 'error', function ( error ) { reject( error ); } );
-        form.on( 'close', function () { resolve( result ); } );
+        form.on('error', function (error) { reject(error); });
+        form.on('close', function () { resolve(result); });
 
-        form.on( 'field', function ( field, value ) { fields[field] = value; } );
-        form.on( 'file' , function ( name, file) {
-            if ( file.originalFilename )
+        form.on('field', function (field, value) { fields[field] = value; });
+        form.on('file', function (name, file) {
+            if (file.originalFilename)
                 files.push({
                     'name': file.originalFilename,
                     'content_type': file.headers['content-type'],
@@ -82,34 +82,38 @@ function parseFormData ( req ) {
                 });
         });
 
-        form.parse( req );
+        form.parse(req);
     });
 }
 
-function cleanTemporaryFiles ( files ) {
-    files.forEach( function ( file ) {
-        fs.unlink( file.path );
+function cleanTemporaryFiles(files) {
+    files.forEach(function (file) {
+        fs.unlink(file.path);
     });
 }
 
 // list bolos at the root route
 router.get('/', function (req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
-    var limit = 3;
-    var skip = 0;
-    
-    boloService.getBolos(limit,skip)
-        .then(function (bolos) {
+    var pageSize = 2;
+    var currentPage = req.query.page || 1;
+
+    boloService.getBolos(pageSize, currentPage)
+        .then(function (result) {
             res.render('bolo-list', {
-                bolos: bolos
+                bolos: result.bolos,
+                paging: {
+                    pages: result.pages,
+                    currentPage: currentPage
+                }
             });
         });
 });
 
 // list archive bolos
 router.get('/archive', function (req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
 
     boloService.getArchiveBolos()
@@ -122,35 +126,35 @@ router.get('/archive', function (req, res) {
 
 // render the bolo create form
 router.get('/create', function (req, res) {
-    res.render( 'create-bolo-form' );
+    res.render('create-bolo-form');
 });
 
 // process bolo creation user form input
-router.post('/create', function(req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
-    var boloService = new BoloService( boloRepository );
+router.post('/create', function (req, res) {
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
+    var boloService = new BoloService(boloRepository);
 
-    parseFormData( req )
-    .then( function ( formDTO ) {
-        var boloDTO = setBoloData( formDTO.fields );
-        var result = boloService.createBolo( boloDTO, formDTO.files );
-        return Promise.all([ result, formDTO ]);
-    })
-    .then( function ( pData ) {
-        if ( pData[1].files.length ) cleanTemporaryFiles( pData[1].files );
-        res.redirect( '/bolo' );
-    })
-    .catch( function ( _error ) {
-        /** @todo send back form data with error message */
-        console.error( '>>> create bolo route error: ', _error );
-        res.redirect( '/bolo/create' );
-    });
+    parseFormData(req)
+        .then(function (formDTO) {
+            var boloDTO = setBoloData(formDTO.fields);
+            var result = boloService.createBolo(boloDTO, formDTO.files);
+            return Promise.all([result, formDTO]);
+        })
+        .then(function (pData) {
+            if (pData[1].files.length) cleanTemporaryFiles(pData[1].files);
+            res.redirect('/bolo');
+        })
+        .catch(function (_error) {
+            /** @todo send back form data with error message */
+            console.error('>>> create bolo route error: ', _error);
+            res.redirect('/bolo/create');
+        });
 });
 
 // render the bolo edit form
 router.get('/edit/:id', function (req, res) {
 
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
 
     boloService.getBolo(req.params.id)
@@ -166,39 +170,39 @@ router.get('/edit/:id', function (req, res) {
 
 // handle requests to process edits on a specific bolo
 router.post('/edit/:id', function (req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
 
-    parseFormData( req )
-    .then( function ( formDTO ) {
-        var boloDTO = setBoloData( formDTO.fields );
-        boloDTO.lastUpdatedOn = getDateTime();
-        var result = boloService.updateBolo( boloDTO, formDTO.files );
-        return Promise.all([ result, formDTO ]);
-    })
-    .then( function ( pData ) {
-        if ( pData[1].files.length ) cleanTemporaryFiles( pData[1].files );
-        res.redirect( '/bolo' );
-    })
-    .catch( function ( _error ) {
-        console.error( '>>> edit bolo route error: ', _error );
-        res.redirect( 'back' );
-    });
+    parseFormData(req)
+        .then(function (formDTO) {
+            var boloDTO = setBoloData(formDTO.fields);
+            boloDTO.lastUpdatedOn = getDateTime();
+            var result = boloService.updateBolo(boloDTO, formDTO.files);
+            return Promise.all([result, formDTO]);
+        })
+        .then(function (pData) {
+            if (pData[1].files.length) cleanTemporaryFiles(pData[1].files);
+            res.redirect('/bolo');
+        })
+        .catch(function (_error) {
+            console.error('>>> edit bolo route error: ', _error);
+            res.redirect('back');
+        });
 
 });
 
 // handle requests to inactivate a specific bolo
 router.post('/archive/:id', function (req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
-    
+
     var activate = false;
-    return boloService.activate( req.params.id, activate )
-        .then( function ( success ) {
-            if ( !success ) {
-                throw new Error( "Bolo not inactivated. Please try again." );
+    return boloService.activate(req.params.id, activate)
+        .then(function (success) {
+            if (!success) {
+                throw new Error("Bolo not inactivated. Please try again.");
             }
-            res.redirect( '/bolo' );
+            res.redirect('/bolo');
         })
         .catch(function (_error) {
             /** @todo redirect and send flash message with error */
@@ -207,16 +211,16 @@ router.post('/archive/:id', function (req, res) {
 });
 
 router.post('/restore/:id', function (req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
-    
+
     var activate = true;
-    return boloService.activate( req.params.id, activate )
-        .then( function ( success ) {
-            if ( !success ) {
-                throw new Error( "Bolo not activated. Please try again." );
+    return boloService.activate(req.params.id, activate)
+        .then(function (success) {
+            if (!success) {
+                throw new Error("Bolo not activated. Please try again.");
             }
-            res.redirect( '/bolo/archive' );
+            res.redirect('/bolo/archive');
         })
         .catch(function (_error) {
             /** @todo redirect and send flash message with error */
@@ -226,15 +230,15 @@ router.post('/restore/:id', function (req, res) {
 
 
 router.post('/delete/:id', function (req, res) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
 
-    return boloService.removeBolo( req.params.id )
-        .then( function ( success ) {
-            if ( !success ) {
-                throw new Error( "Bolo not deleted. Please try again." );
+    return boloService.removeBolo(req.params.id)
+        .then(function (success) {
+            if (!success) {
+                throw new Error("Bolo not deleted. Please try again.");
             }
-            res.redirect( '/bolo' );
+            res.redirect('/bolo');
         })
         .catch(function (_error) {
             // @todo redirect and send flash message with error 
@@ -243,8 +247,8 @@ router.post('/delete/:id', function (req, res) {
 });
 
 // handle requests to view the details of a bolo
-router.get('/details/:id', function ( req, res ) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+router.get('/details/:id', function (req, res) {
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
 
     boloService.getBolo(req.params.id)
@@ -259,20 +263,19 @@ router.get('/details/:id', function ( req, res ) {
 });
 
 // handle requests for bolo attachments
-router.get( '/asset/:boloid/:attname', function ( req, res ) {
-    var boloRepository = AdapterFactory.create( 'persistence', 'cloudant-bolo-repository' );
+router.get('/asset/:boloid/:attname', function (req, res) {
+    var boloRepository = AdapterFactory.create('persistence', 'cloudant-bolo-repository');
     var boloService = new BoloService(boloRepository);
 
-    boloService.getAttachment( req.params.boloid, req.params.attname )
-        .then( function ( attDTO ) {
-            res.type( attDTO.content_type );
-            res.send( attDTO.data );
+    boloService.getAttachment(req.params.boloid, req.params.attname)
+        .then(function (attDTO) {
+            res.type(attDTO.content_type);
+            res.send(attDTO.data);
         });
 });
 
-function ArchiveRestoreBolo(boloId, activate)
-{
-    
-    
+function ArchiveRestoreBolo(boloId, activate) {
+
+
 }
 module.exports = router;
