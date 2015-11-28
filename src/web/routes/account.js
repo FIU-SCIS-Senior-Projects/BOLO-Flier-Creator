ab2a026f3f308ead80d6e82e1cccede0f3f39201
Ryan del Rosario <rdelr011@fiu.edu>
2015-11-28 15:32:01 -0500

    Implement change password router and template.

--- src/web/routes/account.js
+++ src/web/routes/account.js
@@ -8,14 +8,25 @@ var config          = require('../config.js');
 var userService     = new config.UserService( new config.UserRepository() );
 
 
+var FERR            = 'Flash Subject - Error';
+var FMSG            = 'Flash Subject - Message';
+
+
 module.exports = router;
 
 router.get( '/', function ( req, res ) {
-    res.locals.account_nav = 'account-home';
-
     var data = {
+        'account_nav': 'account-home',
         'user': req.user
     };
-
     res.render( 'account-details', data );
+});
+
+router.get( '/password', function ( req, res ) {
+    var data = {
+        'account_nav': 'account-password',
+        'msg': req.flash( FMSG ),
+        'err': req.flash( FERR )
+    };
+    res.render( 'account-password', data );
 });
