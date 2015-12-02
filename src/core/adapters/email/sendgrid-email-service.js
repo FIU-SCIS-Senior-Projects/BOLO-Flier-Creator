ed5eb38263a2e2232548b9a44d6617e4bf33fc73
Ryan del Rosario <rdelr011@fiu.edu>
2015-12-02 12:14:12 -0500

    Create initial sendgrid email service adapter

---
+++ src/core/adapters/email/sendgrid-email-service.js
@@ -0,0 +1,17 @@
+if ( ! process.env.SENDGRID_API_KEY ) {
+    throw new Error(
+        'SendGrid API key not found: SENDGRID_API_KEY should be set.'
+    );
+}
+
+var sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
+
+
+module.exports.send = function ( payload ) {
+    return new Promise( function ( resolve, reject ) {
+        sendgrid.send( payload, function ( err, json ) {
+            if ( err ) reject( err );
+            else resolve( json );
+        });
+    });
+};
