43a6a51f7c8bd173b2e74f000c40075555818a71
Ryan del Rosario <rdelr011@fiu.edu>
2015-12-04 19:02:40 -0500

    Create agency details page

--- src/web/routes/agency.js
+++ src/web/routes/agency.js
@@ -15,6 +15,8 @@ var GFMSG               = config.const.GFMSG;
 
 module.exports = router;
 router.get( '/agency', getListing );
+router.get( '/agency/:id', getAgencyDetails );
+router.get( '/agency/:id/img/:att', getAgencyAttachment );
 
 
 /**
@@ -32,5 +34,29 @@ function getListing ( req, res ) {
         console.error( 'Error at %s >> %s', req.originalUrl, error.message );
         req.flash( GFERR, 'Internal server error occurred. Please try again.' );
         req.redirect( 'back' );
+    });
+}
+
+
+function getAgencyDetails ( req, res ) {
+    var data = {
+        'agency': null
+    };
+
+    agencyService.getAgency( req.params.id ).then( function ( agency ) {
+        data.agency = agency;
+        res.render( 'agency-details', data );
+    }).catch( function ( error ) {
+        console.error( 'Error at %s >> %s', req.originalUrl, error.message );
+        req.flash( GFERR, 'Internal server error occurred. Please try again.' );
+        req.redirect( 'back' );
+    });
+}
+
+
+function getAgencyAttachment ( req, res ) {
+    agencyService.getAttachment( req.params.id, req.params.att ).then( function ( att ) {
+        res.type( att.content_type );
+        res.send( att.data );
     });
 }
